ecef4c55dfa7d1ce89e666b7bd2daf5dc7451e74
Tyler <tylerward1@btinternet.com>
2025-02-10 15:01:40 +0000

    added analytics

--- learn-access-app/src/app/layout.js
+++ learn-access-app/src/app/layout.js
@@ -1,6 +1,7 @@
 import {Geist, Geist_Mono} from "next/font/google";
 import "./globals.css";
 import {ReactQueryProvider} from "@/app/ui/ReactQueryProvider";
+import {Analytics} from "@vercel/analytics/next";
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
@@ -21,9 +22,10 @@ export default function RootLayout({children}) {
 
     return (
         <html lang="en">
-        <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <ReactQueryProvider>{children}</ReactQueryProvider>
-        </body>
+            <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+                <ReactQueryProvider>{children}</ReactQueryProvider>
+                <Analytics/>
+            </body>
         </html>
     );
 }
