c161f0dde483736a89c0f2eefd8ade36489fc133
Tyler <tylerward1@btinternet.com>
2025-02-21 16:47:21 +0000

    began sorting create post endpoint

--- learn-access-app/src/app/api/forum/[levelId]/route.js
+++ learn-access-app/src/app/api/forum/[levelId]/route.js
@@ -17,3 +17,15 @@ export async function GET(req, {params}) {
 
     return Response.json(forum);
 }
+
+export async function POST(req, {params}) {
+
+    if (!await hasSession()) {
+        return Response.json({error: 'You are not authenticated, please login'}, {status: 401});
+    }
+    console.log("user authenticated")
+
+    const levelId = (await params).levelId;
+
+    const user = await getUser();
+}
--- learn-access-app/src/app/lib/DAO/forumDAO.js
+++ learn-access-app/src/app/lib/DAO/forumDAO.js
@@ -1,7 +1,41 @@
 import {sql} from "@vercel/postgres";
 
-const getPostByLevelId = async (levelId) => {
+export const getPostByLevelId = async (levelId) => {
     const result = await sql`
-        
+
     `
+}
+
+export const createPost = async (userId, levelId, files, title, message) => {
+    try {
+        await sql`BEGIN;`;
+
+        const postResult = await sql`
+            INSERT INTO posts (userId, levelId, datetime, title, message)
+            VALUES (${userId}, ${levelId}, NOW(), ${title}, ${message})
+            RETURNING id;
+        `;
+        const postId = postResult.rows[0].id;
+
+        if (files.length > 0) {
+            await Promise.all(
+                files.map(file =>
+                    sql`
+                        INSERT INTO post_files (postId, name, fileType, content)
+                        VALUES (${postId}, ${file.name}, ${file.fileType}, ${file.content});
+                    `
+                )
+            );
+        }
+
+        await sql`COMMIT;`;
+    } catch (error) {
+        try {
+            await sql`ROLLBACK;`;
+        } catch (rollbackError) {
+            console.error("Rollback failed:", rollbackError);
+        }
+        console.error("Transaction failed:", error);
+        throw error;
+    }
 }
