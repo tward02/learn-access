b443ff9a244d2719bb6e3565c53933dff4a86efe
tyler <tylerward1@btinternet.com>
2025-03-06 01:11:14 +0000

    added new level and minor bug fixes

---
+++ learn-access-api/levelTestingSolutions/Level 9 The Tables Have Turned/Initial.js
@@ -0,0 +1,45 @@
+import React from "react";
+import "./styles.css"
+
+export default function App() {
+    return (
+        <table data-testid="table">
+            <thead>
+            <tr>
+                <td></td>
+                <th>Monday</th>
+                <th>Tuesday</th>
+                <th>Wednesday</th>
+                <th>Thursday</th>
+                <th>Friday</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <th>09:00 – 11:00</th>
+                <td>Closed</td>
+                <td>Open</td>
+                <td>Open</td>
+                <td>Closed</td>
+                <td>Closed</td>
+            </tr>
+            <tr>
+                <th>11:00 – 13:00</th>
+                <td>Open</td>
+                <td>Open</td>
+                <td>Closed</td>
+                <td>Closed</td>
+                <td>Closed</td>
+            </tr>
+            <tr>
+                <th>13:00 – 16:00</th>
+                <td>Closed</td>
+                <td>Closed</td>
+                <td>Open</td>
+                <td>Open</td>
+                <td>Open</td>
+            </tr>
+            </tbody>
+        </table>
+    );
+};
